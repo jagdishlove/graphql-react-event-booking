909fc62e5930dcc8d7ea8e10234baf2d2842e1ba
jagdishlove <jagdish.funn@gmail.com>
2022-01-19 16:25:09 +0530

    update app.js

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+
+
+const Event = require("./models/event");
 
 const app = express();
 
@@ -10,9 +14,9 @@ const events = [];
 app.use(bodyParser.json());
 
 app.use(
-    "/graphql",
-    graphqlHTTP({
-        schema: buildSchema(`
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
         type Event{
             _id:ID
             title:String!
@@ -41,27 +45,41 @@ app.use(
               mutation: RootMutation
        }
     `),
-        rootValue: {
-            events: () => {
-                return events;
-            },
-            createEvent: (args) => {
-                const event = {
-                    _id: Math.random().toString(),
-                    title: args.eventInput.title,
-                    description: args.eventInput.description,
-                    price: +args.eventInput.price,
-                    date: args.eventInput.date,
-                };
-                console.log(events);
-                return event;
-
-            }
-        },
-        graphiql: true
-    })
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: (args) => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date),
+        });
+        return event
+          .save()
+          .then((result) => {
+            console.log(result);
+            return { ...result._doc };
+          })
+          .catch((err) => {
+            console.log(err);
+            throw err;
+          });
+        return event;
+      },
+    },
+    graphiql: true,
+  })
 );
 
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.wkvl3.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+  )
+  .then(console.log("mongo connected"))
+  .catch((err) => console.log(err));
+
 app.listen(3001, () => {
-    console.log("server is running in port 3001");
+  console.log("server is running in port 3001");
 });
